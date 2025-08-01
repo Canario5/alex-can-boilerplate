4d10c90c51f47b655bc7b138a5374e6fb1423886
Canario5 <48844307+Canario5@users.noreply.github.com>
2025-08-01 15:29:27 +0200

    chore(commitlint): increase header max-length to 100 chars
    - Increase max-length for commit headers from 72 to 100 characters
     to allow optional ticket number

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -12,7 +12,8 @@ https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/con
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-leading-blank': [RuleConfigSeverity.Error, 'always'], // changed from warning to error
+    'body-leading-blank': [RuleConfigSeverity.Error, 'always'], //* changed from warning to error; https://commitlint.js.org/reference/rules.html#body-leading-blank
+    'header-max-length': [RuleConfigSeverity.Error, 'always', 100], //* increased from 72 to 100 chars to offset optional (ticket number); https://commitlint.js.org/reference/rules.html#header-max-length
   },
 };
 
